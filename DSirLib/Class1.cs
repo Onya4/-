be04826b74ece3d65dedbf20115e792391f4982b
Dar0n <Dar0n@DESKTOP-A3H482S>
2023-05-31 20:03:28 +0300

    Добавьте файлы проекта.

---
+++ DSirLib/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+namespace DSirLib
+{
+    public class Class1
+    {
+        public void Ser(string sems, DateTime dttm)
+        {
+            string sems1 = JsonConvert.SerializeObject(sems + " || " + dttm);
+            File.WriteAllText("C:\\Users\\Dar0n\\Downloads\\игры\\rip.json", sems1);
+        }
+        public string Des()
+        {
+            string sems = File.ReadAllText("C:\\Users\\Dar0n\\Downloads\\игры\\rip.json");
+            string list_hist = JsonConvert.DeserializeObject<string>(sems);
+            return list_hist;
+        }
+    }
+}
---
+++ последняяНадежда/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DSirLib;
+
+namespace последняяНадежда
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Class1 cl1 = new Class1();
+        private List<string> strings = new List<string>() { "A^B", "!A", "A and B or C" };
+        public List<string> Strings { get { return strings; } set { strings = value;} }
+        public MainWindow()
+        {
+            InitializeComponent();
+            boxy.ItemsSource = Strings;
+        }
+
+        private void ent_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime dttm = new DateTime();
+            dttm = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            cl1.Ser(boxy.Text, dttm);
+        }
+
+        private void hist_Click(object sender, RoutedEventArgs e)
+        {
+            var hist = cl1.Des();
+            list_hist.ItemsSource = hist;
+        }
+    }
+}
